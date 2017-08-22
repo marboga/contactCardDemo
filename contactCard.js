b132765e53d258eaab75ba549e8704823ca984ff
Michael Arbogast <marbogast@codingdojo.com>
2017-08-22 14:36:37 -0700

    initial commit

---
+++ contactCard.js
@@ -0,0 +1,32 @@
+function generateCard(fname, lname, desc) {
+  var card =  `
+    <div class="card">
+      <h2>`+ fname +` `+ lname +`</h2>
+      <h6>Click for details</h6>
+      <p style="display: none;">`+ desc +`</p>
+    </div>`
+  console.log(card);
+  $(".cards").append(card)
+}
+
+$(document).ready(function() {
+  console.log("document is ready");
+
+  $("button").click(function(e) {
+    e.preventDefault();
+    console.log("e", e);
+    console.log(this);
+    var fn = $("input[name='first_name']").val();
+    var ln = $("input[name='last_name']").val();
+    var desc = $("textarea[name='description']").val();
+    generateCard(fn, ln, desc)
+  })
+
+  $("div.cards").on("click", ".card", function() {
+    //show the description
+    console.log("i was clicked", this);
+    $(this).children().toggle()
+  })
+
+
+})
